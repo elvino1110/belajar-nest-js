36a4b88e1866bc3126f048341698440e68a3732b
elvino <12050111660@students.uin-suska.ac.id>
2025-06-05 22:38:43 +0700

    Acer Dev

--- prisma/migrations/20250605082607_create_table_users/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "users" (
-    "id" SERIAL NOT NULL,
-    "first_name" VARCHAR(100) NOT NULL,
-    "last_name" VARCHAR(100),
-
-    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
-);
